a6e2518c7279ce4e870419a286e6a039b8e3cadf
brotweg <mikejagerman@gmail.com>
2023-01-23 20:51:09 +0100

    loops

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("hoera!")
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -18,6 +20,14 @@
 // klaar!
  // ==========================================
 
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...");
+    }
+    else {
+        console.log("klaar!");
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -31,6 +41,15 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
+
+for (let i = 0; i < 16; i++) {
+    if (i % 2) {
+        console.log(i + " is oneven");
+    }
+    else {
+        console.log(i + " is even");
+    }
+}
 
 
 // ==========================================
@@ -49,3 +68,49 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i < 10; i++) {
+
+    switch (i) {
+        case 1:
+            console.log(i);
+            break;
+        case 2:
+            console.log(i);
+            break;
+        case 3:
+            console.log(i);
+            break;
+        case 4:
+            console.log(">" + i);
+            break;
+        case 5:
+            console.log(">" + i);
+            break;
+        case 6:
+            console.log(">" + i);
+            break;
+        case 7:
+            console.log(">>" + i);
+            break;
+        case 8:
+            console.log(">>" + i);
+            break;
+        case 9:
+            console.log(">>" + i);
+            break;
+        default:
+            console.log('einde');
+    }
+}
+
+for ( let i = 0; i <= 9; i++ ) {
+    if (i <= 3) {
+        console.log(i)
+    } else if (i <= 6) {
+        console.log(">" + i)
+    } else if (i <= 9) {
+        console.log(">>" + i)
+    } else {
+        console.log("dit kan niet")
+    }
+}
